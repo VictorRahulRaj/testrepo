b2fc31aa4052f6aeaf3c94b56c2bce920ac268a5
Victor <victorrahulraj10@gmail.com>
2024-05-04 17:36:14 +0530

    print update

--- testPython.py
+++ testPython.py
@@ -1,2 +1,9 @@
 #Write Hello World!!
 print('Hello World!!')
+
+## added a new branch ("feature")
+
+print('Hello Git!!!')
+
+it config --global user.email "you@example.com"
+  git config --global user.name "Your Name"
